caef193141b8ba693254e1ccb606c7ca441aa2a7
Kaauany <160200450+Kaauany@users.noreply.github.com>
2024-06-21 10:40:03 -0300

    1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(800, 800);
+     background("black");
+
+}
+
+function draw() {
+ 
+  stroke("blue");
+  fill ("red");
+ 
+  if(mouseIsPressed){
+    circle(mouseX, mouseY, 20, 10);
   }
-  
-  function draw() {
-    background(220);
-  }
+}
